73e54e0dc7cf9afa0f5a25c4665865c3d5b22499
BJKill <brandon.j.kill@gmail.com>
2020-06-20 18:14:53 -0400

    Completed first two sections of Programming Assignment 3. Made histogram and completed 'Best.R' program.

--- best.R
+++ best.R
@@ -1,33 +1,33 @@
 ### Programming Assignment 3
 
 best <- function(state = "character", outcome = "character") {
-        ## Read outcome data
-        data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-        data_split <- split(data, data$State)
+        ## Reads data
+        data1 <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
+        ## check to see if state is valid abbreviation. invalid states stop w/error
         if(is.element(state, state.abb) == FALSE) {
                 stop("invalid state")
         }
-        data_state <- data_split[[state]]
-        #str(data_state)
+        
+        ## assigns column of interest based on outcome. invalid outcomes stop w/ error
         if(outcome == "heart attack") {
-                data_good <- as.numeric(data_split[[state]][[11]])
-                min_rate <- min(data_good)
+                good_col <- 11
         }
         else if(outcome == "heart failure") {
-                data_good <- as.numeric(data_split[[state]][[17]])
-                min_rate <- min(data_good)
+                good_col <- 17
         }
         else if(outcome == "pneumonia") {
-                data_good <- as.numeric(data_split[[state]][[23]])
-                min_rate <- min(data_good)
+                good_col <- 23
         }
         else {
                 stop("invalid outcome")
         }
-        ## Check that state and outcome are valid
-        #str(data_good)
+
         ## Return Hospital name in the state with the lowest 30-day death rate
-        min_rate <- min(data_good)
-        #hos_name <- data_state[which(data_state]
-        
+        data1[,good_col] <- as.numeric(data1[,good_col])
+        data_good <- data1[which(data1$State == state), ]
+        min_rate <- min(data_good[, good_col], na.rm = TRUE)
+        hos_name <- data_good[which(data_good[, good_col] == min_rate), "Hospital.Name"]
+        hos_name <- sort(hos_name)
+        print(hos_name[1])
 }
