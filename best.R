0023b6d885dfa7285ca33d028fc414cf08953f26
BJKill <brandon.j.kill@gmail.com>
2020-06-20 16:24:20 -0400

    reads data and checks for valid inputs. also splits data by state.

--- best.R
+++ best.R
@@ -1,13 +1,5 @@
 ### Programming Assignment 3
 
-#outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-#head(outcome)
-#str(outcome)
-#names(outcome)
-
-#outcome[,11] <- as.numeric(outcome[,11])
-#hist(outcome[,11])
-
 best <- function(state = "character", outcome = "character") {
         ## Read outcome data
         data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
@@ -16,41 +8,26 @@ best <- function(state = "character", outcome = "character") {
                 stop("invalid state")
         }
         data_state <- data_split[[state]]
-        str(data_state)
+        #str(data_state)
         if(outcome == "heart attack") {
                 data_good <- as.numeric(data_split[[state]][[11]])
+                min_rate <- min(data_good)
         }
         else if(outcome == "heart failure") {
                 data_good <- as.numeric(data_split[[state]][[17]])
+                min_rate <- min(data_good)
         }
         else if(outcome == "pneumonia") {
                 data_good <- as.numeric(data_split[[state]][[23]])
+                min_rate <- min(data_good)
         }
         else {
                 stop("invalid outcome")
         }
         ## Check that state and outcome are valid
-        str(data_good)
+        #str(data_good)
         ## Return Hospital name in the state with the lowest 30-day death rate
-        
+        min_rate <- min(data_good)
+        #hos_name <- data_state[which(data_state]
         
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
